11216b4e308d352e358987e01c24960067493547
shengguo.hu <782205103@qq.com>
2018-03-02 16:00:01 +0800

    测试

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\components\BaseController;
+use app\helpers\CurlHelper;
 
 class SiteController extends BaseController
 {
@@ -11,7 +12,63 @@ class SiteController extends BaseController
      */
     function actionIndex()
     {
-        return $this->redirect('swagger-ui/dist/index.html');
+        $eqid = '9ed09f0d0000c22d000000045a964bce';
+        $host = 'referer.bj.baidubce.com';
+        $uri =  '/v1/eqid/'.$eqid;
+        $signString = $this->sign(($uri));
+        $headers[] = 'GET '.$uri.'HTTP/1.1';
+        $headers[] = 'accept-encoding: gzip, deflate';
+        $headers[] = 'x-bce-date: '.$this->getDate();
+        $headers[] = 'host: referer.bj.baidubce.com';
+        $headers[] = 'accept: /';
+        $headers[] = 'connection: keep-alive';
+        $headers[] = 'contenttype: application/json';
+        $headers[] = 'authorization: '.$signString.'';
+        $res = CurlHelper::get('http://'.$host.$uri,$headers);
+        var_dump($res);die;
+        //return $this->redirect('swagger-ui/dist/index.html');
+    }
+
+    function sign($uri)
+    {
+        $secretKey = 'dcd0abfb01f34442b4293c4254937d25';
+        $accessKey = '8136a22f945b44a1b4ed333bb214c1ad';
+        $expireTime = 1800;
+        $preFix = 'bce-auth-v1/'.$accessKey.'/'.$this->getDate().'/'.$expireTime;
+        $http_method = "GET";
+        $canonicalURI = $this->uriEncode($uri,false);
+        $canonicalQueryString = '';
+        $canonicalHeaders = '';
+        $canonicalRequest = $http_method."\n".$canonicalURI."\n".$canonicalQueryString. "\n".$canonicalHeaders;
+        $signKey = strtolower(bin2hex(hash_hmac('sha256', $preFix, $secretKey, true)));
+        $signature = strtolower(bin2hex(hash_hmac('sha256', $signKey, $canonicalRequest, true)));
+        $signString = 'bce-auth-v1/'.$accessKey.'/'.$this->getDate().'/'.$expireTime.'/ /'.$signature;
+        return $signString;
+    }
+
+    function uriEncode($input,$encodeSlash)
+    {
+        $str = '';
+        for($i=0;$i<strlen($input);$i++){
+            $ch = substr($input,$i,1);
+            if (($ch >= 'A' && $ch <= 'Z') || ($ch >= 'a' && $ch <= 'z') || ($ch >= '0' && $ch <= '9') || $ch == '_' || $ch == '-' || $ch == '~' || $ch == '.') {
+                $str .= $ch;
+            }else if ($ch == '/') {
+                $str.=$encodeSlash ? "%2F" : $ch;
+            } else {
+                $str .= $this->hexEncode($ch);
+            }
+        }
+        return $str;
+    }
+
+    function hexEncode($s) {
+        return rawurlencode($s);
+    }
+
+    function getDate()
+    {
+        return date('Y-m-d').'T'.date('H:i:s').'Z';
     }
 
 
---
+++ helpers/CurlHelper.php
@@ -0,0 +1,38 @@
+<?php
+namespace app\helpers;
+
+class CurlHelper
+{
+
+    public static  function get($url,$header='')
+    {
+        $curl = curl_init();
+        //设置抓取的url
+        curl_setopt($curl, CURLOPT_URL,$url);
+        //设置头文件的信息作为数据流输出
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        //设置获取的信息以文件流的形式返回，而不是直接输出。
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        if($header==''){
+            $header [] = "Accept-Language: zh-CN;q=0.8";
+            curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
+        }else{
+            curl_setopt ( $curl, CURLOPT_HTTPHEADER, $header );
+        }
+        //执行命令
+        $data = curl_exec($curl);
+        var_dump(curl_error($curl));die;
+        //关闭URL请求
+        curl_close($curl);
+        //显示获得的数据
+        return $data;
+    }
+
+    public static  function post()
+    {
+
+    }
+
+}
+
+?>
