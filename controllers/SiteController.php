7acf1f0e046782576c2e5b4398c71deb4198df72
shengguo.hu <782205103@qq.com>
2018-03-02 17:18:05 +0800

    测试

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -40,7 +40,6 @@ class SiteController extends BaseController
         $canonicalQueryString = '';
         $canonicalHeaders = 'host:bj.bcebos.com';
         $canonicalRequest = $http_method."\n".$canonicalURI."\n".$canonicalQueryString. "\n".$canonicalHeaders;
-        var_dump($canonicalRequest);die;
         $signKey = strtolower(bin2hex(hash_hmac('sha256', $preFix, $secretKey, true)));
         $signature = strtolower(bin2hex(hash_hmac('sha256', $signKey, $canonicalRequest, true)));
         $signString = 'bce-auth-v1/'.$accessKey.'/'.$this->getDate().'/'.$expireTime.'/host/'.$signature;
