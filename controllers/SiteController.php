70e54c73d834441f8d39fd9938680467a4f9c6e7
shengguo.hu <782205103@qq.com>
2018-03-02 20:24:38 +0800

    test

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -29,7 +29,7 @@ class SiteController extends BaseController
         $headers['connection'] = 'keep-alive';
         $headers['content-type'] = 'application/json';
 
-        $header = ['User-Agent: php test'];
+        $headers = ['User-Agent: php test'];
         $params = array();
         //date_default_timezone_set("PRC");
         $timestamp = new \DateTime();
