12250f585ef2d76ac4c1cd61babf6c8048cf3a9b
shengguo.hu <782205103@qq.com>
2018-04-28 12:31:25 +0800

    fix: 修改bug

--- modules/api/controllers/DownloadController.php
+++ modules/api/controllers/DownloadController.php
@@ -30,8 +30,7 @@ class DownloadController extends ApiController
         if(empty($model)){
             return $this->responseService->error(ResponseHelper::DATA_NOT_FOUND,ResponseHelper::DATA_NOT_FOUND_MSG);
         }
-        $response = Yii::$app->getResponse();
         $url = str_replace('../../',Yii::$app->params['host'],$model->filename);
-        return $response->sendFile($url.'/'.$model->name);
+        return Yii::$app->response->sendFile($url,$model->name);
     }
 }
